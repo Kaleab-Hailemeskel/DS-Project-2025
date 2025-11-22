9480afc9ee216815df9e7c7aecaf80c59fa3a536
Jonathan Leoul <jonathanleoul@gmail.com>
2025-11-22 08:45:03 -0800

    feat: add reusable React components for Spotify clone

---
+++ components/BlurredCard.tsx
@@ -0,0 +1,30 @@
+import {Card} from "@heroui/card"
+import {CardFooter} from "@heroui/card"
+import {Image} from "@heroui/image"
+import { Button } from "@heroui/button";
+import img from "../assets/hero-card.png";
+export default function BlurredCard() {
+  return (
+    <Card isFooterBlurred className="border-none" radius="lg">
+      <Image
+        alt="Woman listing to music"
+        className="object-cover"
+        height={200}
+        src= {img.src}
+        width={200}
+      />
+      <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+        <p className="text-tiny text-white/80">Available soon.</p>
+        <Button
+          className="text-tiny text-white bg-black/20"
+          color="default"
+          radius="lg"
+          size="sm"
+          variant="flat"
+        >
+          Notify me
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
