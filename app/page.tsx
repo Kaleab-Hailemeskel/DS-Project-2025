23eefba934d3fe2e3a8e34f8d00e14e70a75fb46
Jonathan Leoul <jonathanleoul@gmail.com>
2025-11-23 22:40:27 -0800

    Update main page layout and content in app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
       <div className="mt-8">
         <Snippet hideCopyButton hideSymbol variant="bordered">
           <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
+            Register on <Code color="danger">Meta Vibes</Code> it's all free!
           </span>
         </Snippet>
       </div>
