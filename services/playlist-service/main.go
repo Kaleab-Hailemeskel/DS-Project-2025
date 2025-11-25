b77523ee1b5fa861f36fbdeb1c93c50b9d5d4ad4
Izzat Engida <izzatengida7@gmail.com>
2025-11-25 15:20:56 +0300

    inital commit of docker and ngix setup

---
+++ services/playlist-service/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Playlist service is running!")
+	})
+	fmt.Println("Playlist service running on port 8080")
+	http.ListenAndServe(":8080", nil)
+}
---
+++ services/song-service/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Song service is running!")
+	})
+	fmt.Println("Song service running on port 8080")
+	http.ListenAndServe(":8080", nil)
+}
