9417388afd7926ead2022307bb3b7c24effc63ca
Jonathan Leoul <jonathanleoul@gmail.com>
2025-12-04 08:31:34 -0800

    chore: update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,11 +51,11 @@ export default function RootLayout({
               <Link
                 isExternal
                 className="flex items-center gap-1 text-current"
-                href="https://heroui.com?utm_source=next-app-template"
-                title="heroui.com homepage"
+                href="#"
+                title="META VIBES"
               >
                 <span className="text-default-600">Powered by</span>
-                <p className="text-primary">HeroUI</p>
+                <p className="text-danger">META VIBES</p>
               </Link>
             </footer>
           </div>
