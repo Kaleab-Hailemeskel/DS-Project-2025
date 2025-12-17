4e3ffaebb7c80f1037a4e0229a87afe9f2477ac3
Kaleab Hailemeskel <kaleabhailemeskel47@gmail.com>
2025-12-17 21:05:04 +0300

    pagination error fixed

--- services/song-service/internal/usecase/usecase_interface.go
+++ services/song-service/internal/usecase/usecase_interface.go
@@ -10,18 +10,10 @@ type IUploadUsecase interface {
 
 type ISearchEngineUsecase interface {
 	// 1. Primary Search Logic (The core predictive suggestion feature)
-	// This function remains focused on prefix matching for the main query field (Title).
-	SearchSongsByTitlePrefix(titlePrefix string, offset, page string) ([]*domain.Song, error)
+	SearchSongsByTitlePrefix(titlePrefix string, pageNumber, pageLimit string) ([]*domain.Song, error)
 
 	// 2. Search & Filtering Logic (Including Genre/Year)
-
-	// Generalized search function that allows filtering by multiple fields.
-	// This is useful for building a faceted search or a "search filter" UI.
-	// Example: query="Bohemian", filter={"Genre": "Rock", "Year": 1975}
-	FilterSongs(query string, filters map[string]interface{}) ([]*domain.Song, error)
-
-	// // Specialized searches can be added for common use cases:
-	// SearchByGenre(genre string, query string) ([]*domain.Song, error)
+	FilterSongs(query string, filters map[string]interface{}) ([]*domain.Song, error) //! not imped yet
 
 	// 3. Data Synchronization
 	IndexSong(song *domain.Song) error
