29020b1118c872d86eaefbe64e5a3eddaa51570f
Kaleab Hailemeskel <kaleabhailemeskel47@gmail.com>
2025-12-28 16:35:56 +0300

    song db initializer

---
+++ services/song-service/config/init.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS songs (
+    id UUID PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    artist VARCHAR(255) NOT NULL,
+    album VARCHAR(255),
+    duration_sec INTEGER NOT NULL,
+    release_year INTEGER,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    genre VARCHAR(100),
+    image_url TEXT
+);
