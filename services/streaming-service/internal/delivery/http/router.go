80f01870c06c2408f07feeb9502ed44230a0f7a9
Kaleab Hailemeskel <kaleabhailemeskel47@gmail.com>
2025-12-31 11:57:06 +0300

    routing fixed to be accepted from all client

--- services/streaming-service/internal/delivery/http/router.go
+++ services/streaming-service/internal/delivery/http/router.go
@@ -3,6 +3,7 @@ package http
 import (
 	"streaming-service/internal/middleware"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,6 +16,11 @@ func RegisterStreamRoutes(router *gin.RouterGroup, streamController IStreamContr
 
 func InitRouter(streamController IStreamController, middleware middleware.IMiddleware) *gin.Engine {
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders: []string{"Origin", "Content-Type", "Authorization"},
+	}))
 	streamGroup := router.Group("/streams", middleware.AuthUser)
 	RegisterStreamRoutes(streamGroup, streamController)
 	return router
