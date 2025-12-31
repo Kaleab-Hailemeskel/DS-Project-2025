c0df5f62c66c5cf6ace83802983b1c368445d422
Kaleab Hailemeskel <kaleabhailemeskel47@gmail.com>
2025-12-31 12:07:27 +0300

    redis cache added

--- services/streaming-service/internal/repository/init_db.go
+++ services/streaming-service/internal/repository/init_db.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"log"
 	"streaming-service/config"
 	"time"
 
@@ -19,5 +20,17 @@ func InitRedisClient() *redis.Client {
 		Password: config.REDIS_PASSWORD,
 		DB:       config.REDIS_DB,
 	})
+	log.Printf("Initializing Redis client with config: Addr=%s, Password=%s, DB=%d",
+		config.REDIS_ADDR, config.REDIS_PASSWORD, config.REDIS_DB)
+
+	// Context is required for go-redis v8+
+	ctx := context.Background()
+
+	// Perform the Ping
+	_, err := rdb.Ping(ctx).Result()
+	if err != nil {
+		log.Fatalf("💀❌ Could not connect to Redis: %v", err)
+	}
+
 	return rdb
 }
